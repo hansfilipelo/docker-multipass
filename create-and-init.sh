74af4049dd0c4d3be2efa9ad92ea19a3ce9a4872
Hans-Filip Elo <hansfilipelo@gmail.com>
2021-11-12 22:01:59 +0100

    My (hansfilipelo) config

--- create-and-init.sh
+++ create-and-init.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -xe
 
-MEM=2048M
-CPUS=2
-DISK=20G
+MEM=4096M
+CPUS=3
+DISK=40G
 
 echo "Creating and launching instance"
-multipass launch --name primary -c $CPUS -m $MEM -d $DISK --cloud-init cloud-init.yml
+multipass launch --name docker -c $CPUS -m $MEM -d $DISK --cloud-init cloud-init.yml
 
-echo "Mounting $HOME into remote instance: primary"
-multipass mount ${HOME} primary:/Users/${USER}
+echo "Mounting $HOME into remote instance: docker"
+multipass mount ${HOME} docker:/Users/${USER}
 
-PRIMARY_INSTANCE=$(multipass info primary | grep IPv4 | awk '{split($0,a," "); print a[2]}')
+DOCKER_INSTANCE=$(multipass info docker | grep IPv4 | awk '{split($0,a," "); print a[2]}')
 
 echo "Success! in your bash profile add this:"
-echo "export DOCKER_HOST=tcp://$PRIMARY_INSTANCE:2375"
+echo "export DOCKER_HOST=tcp://$DOCKER_INSTANCE:2375"
